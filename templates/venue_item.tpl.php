5279c6f78186f099ee4366564a2c36b989073fa6
Maarten Jacobs <maartenj30@gmail.com>
2011-12-20 23:12:56 +0000

    Better icon handling in template layer

--- templates/venue_item.tpl.php
+++ templates/venue_item.tpl.php
@@ -2,7 +2,8 @@
 // Process Icon, if any
 $icon = ''; // Prevent E_STRICT warning
 if (array_key_exists(0, $venue->categories)) {
-  $icon_src = $venue->categories[0]->icon;
+  $icon_obj = $venue->categories[0]->icon;
+  $icon_src = $icon_obj->prefix . $icon_obj->sizes[0] . $icon_obj->name;
   $icon = '<img src="' . $icon_src . '" />';
 }
 ?>
